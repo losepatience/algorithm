7d94a8248d24e91cec242f6c51e7aaf34ad9f2e1
John Lee <furious_tauren@163.com>
2014-11-16 18:25:07 +0800

    chrome

--- scripts/chrome-cn-support.sh
+++ scripts/chrome-cn-support.sh
@@ -1,16 +1,36 @@
 #! /bin/bash
 
-F=`which google-chrome`
+chrome=`which google-chrome`
+san=/etc/fonts/conf.d/49-sansserif.conf
+default="WenQuanYi Zen Hei"
 
 if [[ ! $? -eq 0 ]]; then
 	echo "install google-chrome first."
-	exit 2
+	exit -1
 fi
 
-grep 'LANG="zh_CN.UTF-8"' $F > /dev/null 2>&1
-
-if [[ $? -eq 0 ]]; then
-	exit 1
+if [ ! -f $san ]; then
+	echo "No such file!"
+	exit -2
 fi
 
-sudo sed -i '/export CHROME_WRAPPER="`readlink -f "$0"`"/aLANG="zh_CN.UTF-8"' $F
+# FIXME
+if [ ! -z $1 ]; then
+	fc-list | awk -F ':' '{print $2}' | grep "$1" > /dev/null 2>&1
+	if [[ ! $? -eq 0 ]]; then
+		echo "No font $1 found"
+		exit -3
+	fi
+	font="$1"
+else
+	font="$default"
+fi
+
+grep 'LANG="zh_CN.UTF-8"' $chrome > /dev/null 2>&1
+if [[ ! $? -eq 0 ]]; then
+	sudo sed -i '/export CHROME_WRAPPER=/aLANG="zh_CN.UTF-8"' $chrome
+fi
+
+sed "/<edit/!b;n;c\\\t\t\t<string>$font</string>" $san > tmp
+sudo mv tmp $san
+
